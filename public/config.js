bb601fc41a7d537ae5265c1a4b276ac7f92bc03a
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-08 17:23:18 +0530

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,5 @@
+// config.js
+window.IRA_CONFIG = {
+  SUPABASE_URL: "https://ehvusinvfwsaxguuebfc.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVodnVzaW52ZndzYXhndXVlYmZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNTcyMzYsImV4cCI6MjA4MjkzMzIzNn0.6W8OrbTz6RhN3P3R0KIN80Ec9GlkoMF4F0-Q4xRyy48"
+};
